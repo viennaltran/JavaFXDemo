f86be7517b4d03d478d7f9f49ea6e8e5af7f7215
Vienna Tran <vitran@mills.edu>
2022-04-09 15:50:26 -0700

    first commit

---
+++ src/application/FXMLController.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.net.*;
+import java.util.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+
+public class FXMLController implements Initializable {
+    @FXML
+    Button myButton;
+
+    @FXML
+    Label myLabel;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        myButton.setText("I am a button!");
+        myLabel.setText("YO!");
+
+    }
+
+}
